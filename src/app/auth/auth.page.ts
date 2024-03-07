6e299a5f69c3a2a9fcc9d7ea4403791c21f3ac8a
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-03-07 15:05:29 +0800

    Add RouterModule import to AuthPage

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,19 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+// import { IonButton } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
 })
 export class AuthPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
