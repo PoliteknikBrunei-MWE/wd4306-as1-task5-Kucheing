8f7edf88ad31087699441e28003a7d12de3a5738
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-03-18 11:12:53 +0800

    Add Router import and constructor injection

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-places',
@@ -11,11 +12,11 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class PlacesPage implements OnInit {
+  
   places: string[] = ['Brunei', 'Tutong', 'Belait', 'Temburong'];
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
-    
+  
+  ngOnInit() {}
+  
   }
-
-}
